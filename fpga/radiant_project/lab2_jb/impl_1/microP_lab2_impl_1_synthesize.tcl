6993d4896882277943aa5f89959d29200fa0d63b
Jason Bowman <jbowman@g.hmc.edu>
2024-09-11 21:27:42 -0700

    lab work

--- fpga/radiant_project/lab2_jb/impl_1/microP_lab2_impl_1_synthesize.tcl
+++ fpga/radiant_project/lab2_jb/impl_1/microP_lab2_impl_1_synthesize.tcl
@@ -1,17 +1,17 @@
 if {[catch {
 
 # define run engine funtion
-source [file join {C:/Lattice Radiant} scripts tcl flow run_engine.tcl]
+source [file join {C:/lscc/radiant/2024.1} scripts tcl flow run_engine.tcl]
 # define global variables
 global para
 set para(gui_mode) "1"
-set para(prj_dir) "C:/Users/spenc/OneDrive/Desktop/microPs/microP_lab2/fpga/radiant_project/lab2_jb"
+set para(prj_dir) "C:/Users/jbowman/Desktop/microP_lab2/fpga/radiant_project/lab2_jb"
 # synthesize IPs
 # synthesize VMs
 # synthesize top design
 file delete -force -- microP_lab2_impl_1.vm microP_lab2_impl_1.ldc
 ::radiant::runengine::run_engine_newmsg synthesis -f "microP_lab2_impl_1_lattice.synproj" -logfile "microP_lab2_impl_1_lattice.srp"
-::radiant::runengine::run_postsyn [list -a iCE40UP -p iCE40UP5K -t SG48 -sp High-Performance_1.2V -oc Industrial -top -w -o microP_lab2_impl_1_syn.udb microP_lab2_impl_1.vm] [list C:/Users/spenc/OneDrive/Desktop/microPs/microP_lab2/fpga/radiant_project/lab2_jb/impl_1/microP_lab2_impl_1.ldc]
+::radiant::runengine::run_postsyn [list -a iCE40UP -p iCE40UP5K -t SG48 -sp High-Performance_1.2V -oc Industrial -top -w -o microP_lab2_impl_1_syn.udb microP_lab2_impl_1.vm] [list C:/Users/jbowman/Desktop/microP_lab2/fpga/radiant_project/lab2_jb/impl_1/microP_lab2_impl_1.ldc]
 
 } out]} {
    ::radiant::runengine::runtime_log $out
--- model_sim/lab2_debug.sv
+++ model_sim/lab2_debug.sv
@@ -32,62 +32,79 @@ module seven_seg_decoder (
 	//initializing variables
 	logic [23:0] state;
 
+
+	// adding 1 to the value of the state to oscillate based on clk
+	//always_ff @(posedge clk) begin
+		//if (reset)
+			//state <= 24'b0;
+		//else
+			//state <= state + 1;
+	//end
 always_ff @(posedge clk or posedge reset) begin
     if (reset) begin
-        state <= state + 1'b1;
-    end else begin
-		state <= 24'b0;
+        state <= 24'b0;
         select <= 2'b00; // Reset select to 00
+    end else begin
+        state <= state + 1'b1;
 
         // Update select based on the MSB of state
-		if (state[15]) begin
+		if (state[23]) begin
             select <= 2'b10; // Select left input
-			case(left)
-                    4'b0000: seg = 7'b0000001;
-                    4'b0001: seg = 7'b1001111;
-                    4'b0010: seg = 7'b0010010;
-                    4'b0011: seg = 7'b0000110;
-                    4'b0100: seg = 7'b1001100;
-                    4'b0101: seg = 7'b0100100;
-                    4'b0110: seg = 7'b0100000;
-                    4'b0111: seg = 7'b0001111;
-                    4'b1000: seg = 7'b0000000;
-                    4'b1001: seg = 7'b0001100;
-                    4'b1010: seg = 7'b0001000;
-                    4'b1011: seg = 7'b1100000;
-                    4'b1100: seg = 7'b0110001;
-                    4'b1101: seg = 7'b1000010;
-                    4'b1110: seg = 7'b0110000;
-                    4'b1111: seg = 7'b0111000;
-                    default: seg = 7'b1111110;
-                endcase
         end else begin
-             select <= 2'b01; // Select right input
-			 case(right)
-                    4'b0000: seg = 7'b0000001;
-                    4'b0001: seg = 7'b1001111;
-                    4'b0010: seg = 7'b0010010;
-                    4'b0011: seg = 7'b0000110;
-                    4'b0100: seg = 7'b1001100;
-                    4'b0101: seg = 7'b0100100;
-                    4'b0110: seg = 7'b0100000;
-                    4'b0111: seg = 7'b0001111;
-                    4'b1000: seg = 7'b0000000;
-                    4'b1001: seg = 7'b0001100;
-                    4'b1010: seg = 7'b0001000;
-                    4'b1011: seg = 7'b1100000;
-                    4'b1100: seg = 7'b0110001;
-                    4'b1101: seg = 7'b1000010;
-                    4'b1110: seg = 7'b0110000;
-                    4'b1111: seg = 7'b0111000;
-                    default: seg = 7'b1111110;
-                endcase
+            select <= 2'b01; // Select right input
         end
     end
 end
 
 
- 
+ //combinational logic to assign bits to the displays
+ always_comb begin
+        case (select)
+            2'b10: // Use left input
+                case(left)
+                    4'b0000: seg = 7'b0000001;
+                    4'b0001: seg = 7'b1001111;
+                    4'b0010: seg = 7'b0010010;
+                    4'b0011: seg = 7'b0000110;
+                    4'b0100: seg = 7'b1001100;
+                    4'b0101: seg = 7'b0100100;
+                    4'b0110: seg = 7'b0100000;
+                    4'b0111: seg = 7'b0001111;
+                    4'b1000: seg = 7'b0000000;
+                    4'b1001: seg = 7'b0001100;
+                    4'b1010: seg = 7'b0001000;
+                    4'b1011: seg = 7'b1100000;
+                    4'b1100: seg = 7'b0110001;
+                    4'b1101: seg = 7'b1000010;
+                    4'b1110: seg = 7'b0110000;
+                    4'b1111: seg = 7'b0111000;
+                    default: seg = 7'b1111110;
+                endcase
+            2'b01: // Use right input
+                case(right)
+                    4'b0000: seg = 7'b0000001;
+                    4'b0001: seg = 7'b1001111;
+                    4'b0010: seg = 7'b0010010;
+                    4'b0011: seg = 7'b0000110;
+                    4'b0100: seg = 7'b1001100;
+                    4'b0101: seg = 7'b0100100;
+                    4'b0110: seg = 7'b0100000;
+                    4'b0111: seg = 7'b0001111;
+                    4'b1000: seg = 7'b0000000;
+                    4'b1001: seg = 7'b0001100;
+                    4'b1010: seg = 7'b0001000;
+                    4'b1011: seg = 7'b1100000;
+                    4'b1100: seg = 7'b0110001;
+                    4'b1101: seg = 7'b1000010;
+                    4'b1110: seg = 7'b0110000;
+                    4'b1111: seg = 7'b0111000;
+                    default: seg = 7'b1111110;
+                endcase
+			default: seg = 7'b1111111;
+        endcase
+    end
+
+
 
 endmodule
 ///////////////////////////////////////////
@@ -103,4 +120,5 @@ module binary_disp_decoder(
 	always_ff @(posedge clk) begin
 		led = right + left;
 	end
+	
 endmodule
